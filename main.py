d443517f6c807f2f7e4c0aead533c37f12bb4336
Sindre Osnes <sindreosnes.git@gmail.com>
2021-08-30 12:33:02 +0200

    Add profile argument

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='CLI for helping set up databricks.')
+parser.add_argument('--profile', type=str, help='The databricks cli profile to use')
 
 if __name__ == '__main__':
     args = parser.parse_args()
