4760c2f67f646fef27dda014d29f0573af8b8d6b
Sindre Osnes <sindreosnes.git@gmail.com>
2021-08-30 12:43:07 +0200

    Add secret scope parser

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ cluster_parser = subparsers.add_parser('cluster', help='Cluster commands')
 cluster_parser.add_argument('--profile', type=str, help='The databricks cli profile to use')
 cluster_parser.add_argument('--name', type=str, help='The the cluster name')
 
+scope_parser = subparsers.add_parser('cluster', help='Secret scope commands')
+scope_parser.add_argument('--profile', type=str, help='The databricks cli profile to use')
+scope_parser.add_argument('--key-vault', type=str, help='The the key vault name')
+scope_parser.add_argument('--resource-id', type=str, help='The the key vault resource id')
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
