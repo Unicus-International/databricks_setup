850f1082d611f0e0561aa0aeeea85b632dc4c4ec
Sindre Osnes <sindreosnes.git@gmail.com>
2021-08-30 22:56:28 +0200

    Add setup with executable

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dbricks-cli-setup',
+    name='dbricks_setup',
     description='Python module for generating a wrapper around the databricks-cli for setting up clusters and secret scopes.',
     license="MIT",
     url="https://github.com/Unicus-International/databricks_setup",
@@ -9,10 +9,15 @@ setup(
     author_email='sindreosnes.git@gmail.com',
     version='0.0.1',
     include_package_data=True,
-    packages=find_packages(exclude=['tests*']),
+    packages=find_packages( exclude=['tests*']),
     install_requires=[
         'databricks-cli',
     ],
-    test_suite='tests.dbricks_cli_setup_tests',
+    entry_points={
+        'console_scripts': [
+            'dbricks_setup=dbricks_setup._cli:cli'
+        ]
+    },
+    test_suite='tests.dbricks_setup_tests',
 
 )
