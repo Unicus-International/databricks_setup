c81ad2563e1f83408e498dd3403cdcdd1b08b01a
Sindre Osnes <sindreosnes.git@gmail.com>
2021-08-30 13:57:48 +0200

    Add group query

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
-
+import subprocess
+import json
 
 parser = argparse.ArgumentParser(description='CLI for helping set up databricks.')
 parser.add_argument('--profile', type=str, help='The databricks cli profile to use')
@@ -9,7 +10,7 @@ cluster_parser = subparsers.add_parser('cluster', help='Cluster commands')
 cluster_parser.add_argument('--profile', type=str, help='The databricks cli profile to use')
 cluster_parser.add_argument('--name', type=str, help='The the cluster name')
 
-scope_parser = subparsers.add_parser('cluster', help='Secret scope commands')
+scope_parser = subparsers.add_parser('scope', help='Secret scope commands')
 scope_parser.add_argument('--profile', type=str, help='The databricks cli profile to use')
 scope_parser.add_argument('--key-vault', type=str, help='The the key vault name')
 scope_parser.add_argument('--resource-id', type=str, help='The the key vault resource id')
@@ -17,4 +18,13 @@ scope_parser.add_argument('--resource-id', type=str, help='The the key vault res
 
 if __name__ == '__main__':
     args = parser.parse_args()
+
+    # query what groups are available
+    group_query = 'databricks groups2 list'
+    if args.profile is not None:
+        group_query += f' --profile {args.profile}'
+
+    sp = subprocess.run(group_query, capture_output=True)
+    sp.check_returncode()
+    groups = json.loads(sp.stdout)
     print(args)
