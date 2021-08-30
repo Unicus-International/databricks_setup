fd365c95ff5c0a0c0f7c9e0cab094d3b5b293cd9
Sindre Osnes <sindreosnes.git@gmail.com>
2021-08-30 19:18:27 +0200

    Add cluster list and spark version extraction

--- main.py
+++ main.py
@@ -416,3 +416,57 @@ if __name__ == '__main__':
                 logging.warning(f'Deleting scope {scope_name}')
                 sp = subprocess.run(delete_query, capture_output=True)
                 sp.check_returncode()
+
+    if args.which == 'cluster_update':
+        cluster_name = args.name.lower()
+
+        # Query what clusters exists
+        cluster_query = 'databricks clusters list'
+        cluster_query += f' --profile {profile}'
+
+        # Run and enforce success
+        sp = subprocess.run(cluster_query, capture_output=True)
+        sp.check_returncode()
+
+        # Extract the existing scopes
+        cluster_lines = [l.strip('\r') for l in sp.stdout.decode().split('\n')]
+        cluster_lines = [l for l in cluster_lines if l.replace('-', '').strip()]
+        cluster_lines = [[elem for elem in l.split(' ') if elem] for l in cluster_lines]
+        existing_clusters = [{'cluster_id': cluster[0], 'name': cluster[1], 'status': cluster[2]} for cluster in
+                             cluster_lines]
+
+        # Get the clusters matching the desired name
+        matching_clusters = [
+            cluster
+            for cluster
+            in existing_clusters
+            if cluster['name'].lower() == cluster_name
+        ]
+
+        # Get the current spark versions
+        spark_query = 'databricks clusters spark-versions'
+        spark_query += f' --profile {profile}'
+
+        # Run and enforce success
+        sp = subprocess.run(spark_query, capture_output=True)
+        sp.check_returncode()
+
+        # Filter the spark versions
+        spark_versions = json.loads(sp.stdout)['versions']
+        valid_versions = [
+            v
+            for v
+            in spark_versions
+            if (
+                    'gpu' not in v['key']
+                    and 'cpu' not in v['key']
+                    and 'photon' not in v['key']
+                    and 'apache' not in v['key']
+            )
+        ]
+        valid_versions = sorted(valid_versions, key=lambda x: x['key'])
+
+        # Get the last version
+        last_version = valid_versions[-1]
+
+        print(last_version)
